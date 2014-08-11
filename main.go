3e2c0faf5b35ee73b96f7117ea0028b3777f2404
Takatoshi Matsumoto <toqoz403@gmail.com>
2014-08-12 05:27:35 +0900

    Fix flag.Usage

--- main.go
+++ main.go
@@ -18,8 +18,8 @@ Usage:
 The commands are:
 
 	importable             show import paths of importable packages
-	dropable [paths...]    show import paths of dropable packages in file
-	unused [paths...]      show import paths of unused packages in file.
+	dropable [path]        show import paths of dropable packages in file
+	unused [path]          show import paths of unused packages in file.
 	fmt [flags] [paths...] drop unused packages and format file(ast as gofmt).
 	                       if you want to know options for goimps fmt, please run "goimps fmt -h".
 
